ca7d8d4a73d5c13f37b025e22c479cc74380ac61
Simone Farre <simon.farre.cx@gmail.com>
2021-02-17 17:38:08 +0100

    added usage examples and usage notes to tests/main.cpp

--- tests/main.cpp
+++ tests/main.cpp
@@ -5,20 +5,67 @@
 #include <array>
 #include <vector>
 #include <iostream>
+#include <ranges>
+#include <algorithm>
+#include <cctype>
+
+/// Notice that using my utils, when iterating you shall not write for(auto& ...), as this will be a compiler error
+/// Because in C++ we have no (as far as I know, and I am *certainly* no expert) decent way of saying (someRefType1, someRefType2)
+/// meaning, for(auto& ...) will try to get a reference to the pair we return from zip, holding the references to the container elements.
+/// So this instantly becomes counter-intuitive if one is writing C++, since we are used to say "for(auto& ...)" if we want to mutate the elements
+/// Now, we should instead write for(auto ...) or for(auto&& ). Notice that for(auto...) shouldn't make any copies of the elements, so it's all reference based. I hope. Lol
 
 int main() {
     std::array<char, 8> A{'h', 'l', 'o', ' ', 'o', 'l', '!', '_'}; // last _ char to show that zip(..) ends as soon as one container runs out
     std::array<char, 7> B{'e', 'l', ',', 'w', 'r', 'd', '!'}; 
 
+    // zip-iterating over A and B
     for(auto&& [a, b] : cxutils::zip(A, B)) {
         std::cout << a << b;
     }
+    // output: "hello, world!!"
     std::cout << std::endl;
 
     std::vector<int> ints{3, 2, 1};
 
+    // enumerating over ints
     for(auto&& [index, el] : cxutils::enumerate(ints)) {
         std::cout << "index: " << index << ", value: " << el << std::endl;
     }
+    // output: 
+    // index: 0, value: 3
+    // index: 1, value: 2
+    // index: 2, value: 1
 
+    std::array<char, 6> A2{'h', 'l', 'o', ' ', 'o', 'l'}; 
+    std::array<char, 7> B2{'e', 'l', ',', 'w', 'r', 'd', '!'}; // last ! char to show that zip(..) ends as soon as one container runs out of elements
+
+    for(auto&& [a, b] : cxutils::zip(A2, B2)) {
+        std::cout << a << b;
+    }
+    // output: "hello, world"
+    std::cout << std::endl;
+    std::vector<int> i{1,2,3};
+    std::vector<double> d{3.1,2.2,1.3};
+
+    // zip-iterating over two containers with different "Container::value_type"s
+    for(auto&& [a, b] : cxutils::zip(i, d)) {
+        std::cout << a << ", " << b << std::endl;
+    }
+    // output: 
+    // 1, 3.1
+    // 2, 2.2
+    // 3, 1.3
+
+    // mutating the containers A and B:
+    for(auto&& [a, b] : cxutils::zip(A, B)) {
+        a = std::toupper(a);
+        b = std::toupper(b);
+    }
+    // after uppercase
+    for(auto&& [a, b] : cxutils::zip(A, B)) {
+        std::cout << a << b;
+    }
+    // output: "HELLO, WORLD!!"
+    std::cout << std::endl;
 }
