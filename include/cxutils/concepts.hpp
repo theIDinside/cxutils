caffdea4b108415bfec206f22592713265346f61
Simon Farre <simon.farre.cx@gmail.com>
2021-02-25 12:29:47 +0100

    Added size() and value_type as constraints to the concept Iterable, for it to be usable in cxutils::transform

--- include/cxutils/concepts.hpp
+++ include/cxutils/concepts.hpp
@@ -7,8 +7,10 @@ concept Iterable = requires(Container c) {
     std::end(c);
     c.begin();
     c.end();
+    c.size();
     // this is, so we can remove & and && from the type. so,
     // std::array<char, 4>& -> std::array<char, 4> since that is what we care for
     typename std::decay_t<Container>::iterator;
     typename std::decay_t<Container>::reference;
+    typename std::decay_t<Container>::value_type;
 };
