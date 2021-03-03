37b43d13c4ac6d8f2c7f4c4c289545852904441d
Simon Farre <simon.farre.cx@gmail.com>
2021-03-03 14:39:27 +0100

    Changed ElementReferenceOf to do SFINAE, so that the reference type is either reference or const_reference, depending on the template type passed in, changed IteratorOf to work in the same way.

--- include/cxutils/concepts.hpp
+++ include/cxutils/concepts.hpp
@@ -12,12 +12,15 @@ concept Iterable = requires(Container c) {
     // this is, so we can remove & and && from the type. so,
     // std::array<char, 4>& -> std::array<char, 4> since that is what we care for
     typename std::decay_t<Container>::iterator;
+    typename std::decay_t<Container>::const_iterator;
     typename std::decay_t<Container>::reference;
+    typename std::decay_t<Container>::const_reference;
     typename std::decay_t<Container>::value_type;
 };
 
-template <Iterable I> using IteratorOf = typename std::decay_t<I>::iterator;
 template <Iterable I>
-using ElementReferenceOf = typename std::decay_t<I>::reference;
+using IteratorOf          = std::conditional_t<std::is_const_v<I>, typename std::decay_t<I>::const_iterator, typename std::decay_t<I>::iterator>;
+template <Iterable I>
+using ElementReferenceOf  = std::conditional_t<std::is_const_v<I>, typename std::decay_t<I>::const_reference, typename std::decay_t<I>::reference>;
 template <Iterable I>
 using ElementValueTypeOf = typename std::decay_t<I>::value_type;
